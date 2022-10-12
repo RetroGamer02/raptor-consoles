8e902d683b147ffc2a685d835c479d6c131900a5
Devin <70994866+RetroGamer02@users.noreply.github.com>
2022-10-12 09:57:48 -0400

    Raise audio quality + Even more speed

--- src/rap.cpp
+++ src/rap.cpp
@@ -177,7 +177,7 @@ void RAP_Bday(void)
 
 void InitScreen(void)
 {
-    printf("RAPTOR: Call Of The Shadows V1.2\n(c)1994 Cygnus Studios\nRaptor3DS: V0.6.2\n");
+    printf("RAPTOR: Call Of The Shadows V1.2\n(c)1994 Cygnus Studios\nRaptor3DS: V0.6.3\n");
 }
 
 void ShutDown(int a1)
