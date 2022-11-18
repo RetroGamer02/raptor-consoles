f48a0ada7cda363633b076a36f5143ba0b938cf8
Devin <70994866+RetroGamer02@users.noreply.github.com>
2022-11-18 00:11:25 -0500

    Remove unneeded files

--- src/tonccpy.c.d
+++
@@ -1,5 +0,0 @@
-C:/Users/Devin/Documents/nxdk/samples/Raptor-xbox/src/tonccpy.obj: \
-  C:/Users/Devin/Documents/nxdk/samples/Raptor-xbox/src/tonccpy.c \
-  C:/Users/Devin/Documents/nxdk/samples/Raptor-xbox/src/tonccpy.h
-
-C:/Users/Devin/Documents/nxdk/samples/Raptor-xbox/src/tonccpy.h:
